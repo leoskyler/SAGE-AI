f458ff79b1af28710a59e5764760a5f56144ef5c
leoskyler <leoskyler33@gmail.com>
2026-06-24 00:33:29 +0000

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,16 @@
+// Firebase SDK Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
+const firebaseConfig = {
+  apiKey:"AIzaSyAY4zcI7_zOvcXH1kxQi-Uu0yvnVTmOE6g"
+  authDomain: "sage-ai-1e078.firebaseapp.com",
+  projectId: "sage-ai-1e078",
+  storageBucket: "sage-ai-1e078.firebasestorage.app",
+  messagingSenderId: "831909949652",
+  appId: "1:831909949652:web:298ce0788d3a3070bf8b41"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+console.log("Firebase Connected");
