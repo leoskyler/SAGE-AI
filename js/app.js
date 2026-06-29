ddfb8ef6ebf1234bd58ebeecda816752d9e3f15d
leoskyler <leoskyler33@gmail.com>
2026-06-29 22:56:46 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -296,7 +296,8 @@ Instructions:
         }
     ]
 };
-
+alert("Before fetch");
+  
     const response =
         await fetch(
             `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
