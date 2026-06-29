dba7198df9eb4e6ad0d340a62605d09638a1abf3
leoskyler <leoskyler33@gmail.com>
2026-06-29 23:34:59 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -256,7 +256,7 @@ function streamText(element, text, speed = 15) {
 async function generateReply(message) {
 
     const apiKey =
-        "AQ.Ab8RN6Jn-8klgIUZamEOrR4MmHN3NNptyPNPOfJQVjrdnE3vNg";
+        "AQ.Ab8RN6IhqtHeKsdTEnGyBcVzcm5iT1Onw6Lad7eYXNfvAzG15A";
 
     const body = {
     contents: [
@@ -298,19 +298,17 @@ Instructions:
 };
 alert("Before fetch");
   
-    const response =
-    await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
-        {
-            method: "POST",
-
-            headers: {
-                "Content-Type": "application/json"
-            },
-
-            body: JSON.stringify(body)
-        }
-    );
+    const response = await fetch(
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent",
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-goog-api-key": apiKey
+        },
+        body: JSON.stringify(body)
+    }
+);
 
 alert("After fetch");
 alert("Status: " + response.status);
